5e9e902cc92bbd41e08dd0e2c0b78c5e330f8cb2
dextar34 <weknownothingbutheknows23@gmail.com>
2024-07-08 19:11:08 +0600

    small pic h w fixed

--- src/components/layer/ImageGallery.jsx
+++ src/components/layer/ImageGallery.jsx
@@ -8,7 +8,8 @@ const ImageGallery= ({ images }) => {
             <div className="bigPic w-[587px] h-[691px] border-[#EDF0F8] rounded-2xl">
                 <img src={activeImg} alt="Active" className="w-full h-full object-cover rounded-2xl border-[#EDF0F8]" />
             </div>  
-            <div className="smallPic flex gap-x-5 pt-8 w-[115px] h-[115px] object-contain">
+            <div className="smallPic">
+                <div className="img  flex gap-x-5 pt-8 w-[115px] h-[115px]">
                 {images.map((img, index) => (
                     <img
                         key={index}
@@ -16,12 +17,13 @@ const ImageGallery= ({ images }) => {
                         onClick={() => setActiveImg(img)}
                         className={
                             activeImg == img
-                                ? 'border-[3px] border-[#3A4980] rounded-2xl'
-                                : 'rounded-2xl border-[3px] border-transparent'
+                                ? 'border-[3px] border-[#3A4980] rounded-2xl object-cover w-full h-full aspect-square' 
+                                : 'rounded-2xl border-[3px] border-transparent object-cover w-full h-full aspect-square'
                         }
                         alt={`Thumbnail ${index}`}
                     />
                 ))}
+                </div>
             </div>
         </div>
     );
--- src/components/layer/ProductDetails.jsx
+++ src/components/layer/ProductDetails.jsx
@@ -14,15 +14,15 @@ const ProductDetails = ({productName,studioName}) => {
                     <h2 className="font-semibold text-2xl">{productName}</h2>
                 </div>
                 <div className="icons flex items-center  justify-between gap-3">
-                    <div className="icontxt text-[#D46F77] bg-[#FFF0F0] rounded-[10px]  flex gap-x-2 py-2 items-center px-2.5">
+                    <div className="icontxt text-[#dcb9b9] bg-[#D46F77] rounded-[10px]  flex gap-x-2 py-2 items-center px-2.5 transition-all duration-400 hover:bg-[#fdb8b8] hover:text-black">
                         <CiHeart />
                         <p className='text-base font-semibold'>109</p>
                     </div>
-                    <div className="bg-[#b4c2f7] rounded-[10px] px-2.5 py-2">  
+                    <div className="bg-[#b4c2f7] rounded-[10px] px-2.5 py-2 hover:bg-[#cbd6fe] transition-all duration-400">  
                     <CiBookmark color='#3A4980' size='25'/>
                     </div>
-                    <div className="bg-[#b4c2f7] rounded-[10px] px-2.5 py-2">  
-                        <CiShare2 color='#3A4980' size='25' />
+                    <div className="bg-[#b4c2f7] rounded-[10px] px-2.5 py-2 hover:bg-[#cbd6fe] transition-all duration-400">  
+                        <CiShare2  color='#3A4980' size='25' />
                     </div>
                 </div>
             </div>
